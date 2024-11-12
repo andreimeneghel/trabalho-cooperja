f91fa79405ad951984cf3cda44588c38ceebbf82
Andrei <andreimeneghel@unesc.net>
2024-11-12 14:23:04 -0300

    2 parte

--- php/blog/rotas.php
+++ php/blog/rotas.php
@@ -1,54 +1,65 @@
 <?php
 
 use Pecee\SimpleRouter\SimpleRouter;
+use Pecee\Http\Request;
 use sistema\Suporte\Helpers;
 
-
-
-
 try {
+    SimpleRouter::get('/', function() {
+        require 'templates/site/views/loginUsuario.php';
+    });
 
+    SimpleRouter::get('/assets/{path}', function (Request $request) {
+        $path = $request->getParam('path');
+        $file = __DIR__ . '/templates/site/assets/' . $path;
+    
+        if (file_exists($file)) {
+            $mimeType = mime_content_type($file);
+            header("Content-Type: {$mimeType}");
+            header("Cache-Control: public, max-age=31536000");
+            readfile($file);
+            exit;
+        } else {
+            http_response_code(404);
+            echo 'File not found';
+        }
+    })->where(['path' => '.*']);
 
-SimpleRouter::get('/', require 'templates/site/views/loginUsuario.html');
-// SimpleRouter::get(URL_SITE.'sobre-nos','SiteControlador@sobre');
-// SimpleRouter::get(URL_SITE.'post/{id}','SiteControlador@post');
-// SimpleRouter::get(URL_SITE.'categoria/{id}','SiteControlador@categoria');
-// SimpleRouter::post(URL_SITE.'buscar','SiteControlador@buscar');
+    // SimpleRouter::get(URL_SITE.'sobre-nos','SiteControlador@sobre');
+    // SimpleRouter::get(URL_SITE.'post/{id}','SiteControlador@post');
+    // SimpleRouter::get(URL_SITE.'categoria/{id}','SiteControlador@categoria');
+    // SimpleRouter::post(URL_SITE.'buscar','SiteControlador@buscar');
 
-// SimpleRouter::get(URL_SITE.'404', 'SiteControlador@erro404');
+    // SimpleRouter::get(URL_SITE.'404', 'SiteControlador@erro404');
 
-// SimpleRouter::group(['namespace' => 'Admin'], function () {
-//     SimpleRouter::get(URL_ADMIN.'dashboard', 'AdminDashboard@dashboard');
+    // SimpleRouter::group(['namespace' => 'Admin'], function () {
+    //     SimpleRouter::get(URL_ADMIN.'dashboard', 'AdminDashboard@dashboard');
 
-//     // ADMIN POSTS
-//     SimpleRouter::get(URL_ADMIN.'posts/listar', 'AdminPosts@listar');
-//     SimpleRouter::match(['get','post'], URL_ADMIN.'posts/cadastrar',
-//     'AdminPosts@cadastrar');
-//     SimpleRouter::match(['get','post'], URL_ADMIN.'posts/editar/{id}',
-//     'AdminPosts@editar');
-//     SimpleRouter::get(URL_ADMIN.'posts/deletar/{id}',
-//     'AdminPosts@deletar');
+    //     // ADMIN POSTS
+    //     SimpleRouter::get(URL_ADMIN.'posts/listar', 'AdminPosts@listar');
+    //     SimpleRouter::match(['get','post'], URL_ADMIN.'posts/cadastrar',
+    //     'AdminPosts@cadastrar');
+    //     SimpleRouter::match(['get','post'], URL_ADMIN.'posts/editar/{id}',
+    //     'AdminPosts@editar');
+    //     SimpleRouter::get(URL_ADMIN.'posts/deletar/{id}',
+    //     'AdminPosts@deletar');
 
-//     // ADMIN CATEGORIAS
-//     SimpleRouter::get(URL_ADMIN.'categorias/listar', 'AdminCategorias@listar');
-//     SimpleRouter::match(['get','post'], URL_ADMIN.'categorias/cadastrar',
-//      'AdminCategorias@cadastrar');
-//      SimpleRouter::match(['get','post'], URL_ADMIN.'categorias/editar/{id}',
-//      'AdminCategorias@editar');
-//      SimpleRouter::get(URL_ADMIN.'categorias/deletar/{id}',
-//      'AdminCategorias@deletar');
-// });
+    //     // ADMIN CATEGORIAS
+    //     SimpleRouter::get(URL_ADMIN.'categorias/listar', 'AdminCategorias@listar');
+    //     SimpleRouter::match(['get','post'], URL_ADMIN.'categorias/cadastrar',
+    //      'AdminCategorias@cadastrar');
+    //      SimpleRouter::match(['get','post'], URL_ADMIN.'categorias/editar/{id}',
+    //      'AdminCategorias@editar');
+    //      SimpleRouter::get(URL_ADMIN.'categorias/deletar/{id}',
+    //      'AdminCategorias@deletar');
+    // });
 
+    SimpleRouter::start();
 
-SimpleRouter::start();
-
-
-}   catch(Pecee\SimpleRouter\Exceptions\NotFoundHttpException $ex){
-    if(Helpers::localhost())
-    {
+} catch(Pecee\SimpleRouter\Exceptions\NotFoundHttpException $ex) {
+    if(Helpers::localhost()) {
         echo $ex;
-    }
-    else {
+    } else {
         Helpers::redirecionar('404');
     }
 }
