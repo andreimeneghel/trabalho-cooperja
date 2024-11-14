4ee7f62ffbd44c59ecf5e5a5fbcb548e463ac936
Andrei <andreimeneghel@unesc.net>
2024-11-14 14:41:43 -0300

    outra parte dos logins arrumado

--- php/blog/rotas.php
+++ php/blog/rotas.php
@@ -21,51 +21,15 @@ try {
         require 'templates/site/views/alunos.php';
     });
 
-    SimpleRouter::get('/configuracoes', function() {
-        require 'templates/site/views/configuracoes.php';
-    });
-
     SimpleRouter::get('/academico', function() {
         require 'templates/site/views/academicos.php';
     });
 
-    SimpleRouter::get('/academico/informacoes', function() {
-       require 'templates/site/views/tabela_academicos.php';
-    });
-
-
-    SimpleRouter::get('/mudarcadprof', function() {
+    SimpleRouter::get('/configuracoes', function() {
         require 'templates/site/views/configuracaoProf.php';
     });
 
-    // SimpleRouter::get(URL_SITE.'sobre-nos','SiteControlador@sobre');
-    // SimpleRouter::get(URL_SITE.'post/{id}','SiteControlador@post');
-    // SimpleRouter::get(URL_SITE.'categoria/{id}','SiteControlador@categoria');
-    // SimpleRouter::post(URL_SITE.'buscar','SiteControlador@buscar');
 
-    // SimpleRouter::get(URL_SITE.'404', 'SiteControlador@erro404');
-
-    // SimpleRouter::group(['namespace' => 'Admin'], function () {
-    //     SimpleRouter::get(URL_ADMIN.'dashboard', 'AdminDashboard@dashboard');
-
-    //     // ADMIN POSTS
-    //     SimpleRouter::get(URL_ADMIN.'posts/listar', 'AdminPosts@listar');
-    //     SimpleRouter::match(['get','post'], URL_ADMIN.'posts/cadastrar',
-    //     'AdminPosts@cadastrar');
-    //     SimpleRouter::match(['get','post'], URL_ADMIN.'posts/editar/{id}',
-    //     'AdminPosts@editar');
-    //     SimpleRouter::get(URL_ADMIN.'posts/deletar/{id}',
-    //     'AdminPosts@deletar');
-
-    //     // ADMIN CATEGORIAS
-    //     SimpleRouter::get(URL_ADMIN.'categorias/listar', 'AdminCategorias@listar');
-    //     SimpleRouter::match(['get','post'], URL_ADMIN.'categorias/cadastrar',
-    //      'AdminCategorias@cadastrar');
-    //      SimpleRouter::match(['get','post'], URL_ADMIN.'categorias/editar/{id}',
-    //      'AdminCategorias@editar');
-    //      SimpleRouter::get(URL_ADMIN.'categorias/deletar/{id}',
-    //      'AdminCategorias@deletar');
-    // });
 
     SimpleRouter::start();
 
