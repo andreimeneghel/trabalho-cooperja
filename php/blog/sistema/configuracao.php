bb406cfd13acf5753d6a54661eae094df631a251
Leandro <leandrocorneo5@unesc.net>
2024-11-14 16:05:03 -0300

    asgasgas

--- php/blog/sistema/configuracao.php
+++ php/blog/sistema/configuracao.php
@@ -2,10 +2,6 @@
 //define o horario
 date_default_timezone_set('America/Sao_Paulo');
 
-//informações do site
-define('SITE_NOME','Cooperja');
-define('SITE_DESCRIÇÃO','Cooperja SLA');
-
 //urls do sistema
 define('URL_PRODUCAO','http://localhost:8000');
 define('URL_DESENVOLVIMENTO','http://localhost:8000');
